d447f2718aabce09bff3f50cb22ae842d8af776e
centuriin <centuriin@yandex.ru>
2026-03-25 15:15:52 +0300

    added allowed transition component

---
+++ Common.Tests/AllowedTransitionComponent.Tests.cs
@@ -0,0 +1,40 @@
+﻿using Centuriin.CardGame.Core.Common.Components;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace Centuriin.CardGame.Core.Common;
+
+public sealed class AllowedTransitionComponentTests
+{
+    [Fact]
+    public void CanCopy()
+    {
+        // Arrange
+        var component = new AllowedTransitionComponent<ZoneId>(
+            new HashSet<ZoneId> { new(1) });
+
+        // Act
+        var copy = component.Copy();
+
+        // Assert
+        copy.Should().BeEquivalentTo(component);
+    }
+
+    [Fact]
+    public void CopyCanNotChangeOriginal()
+    {
+        // Arrange
+        var set = new HashSet<ZoneId> { new(1) };
+        var component = new AllowedTransitionComponent<ZoneId>(set);
+
+        var copy = (AllowedTransitionComponent<ZoneId>)component.Copy();
+
+        // Act
+        copy.AllowedFrom.Add(new ZoneId(999));
+
+        // Assert
+        copy.Should().NotBeEquivalentTo(component);
+    }
+}
---
+++ Common/Components/AllowedTransitionComponent.cs
@@ -0,0 +1,19 @@
+﻿namespace Centuriin.CardGame.Core.Common.Components;
+
+public sealed record class AllowedTransitionComponent<TFromId> : ComponentBase
+    where TFromId : IEquatable<TFromId>
+{
+    public HashSet<TFromId> AllowedFrom { get; }
+
+    public AllowedTransitionComponent(HashSet<TFromId> allowedFrom)
+    {
+        ArgumentNullException.ThrowIfNull(allowedFrom);
+        AllowedFrom = allowedFrom;
+    }
+
+    private AllowedTransitionComponent(AllowedTransitionComponent<TFromId> other)
+        : base(other)
+    {
+        AllowedFrom = other.AllowedFrom.ToHashSet();
+    }
+}
