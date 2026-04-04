57a2b5b4c09511aafc4e418b7f50a99c8dfd7afb
centuriin <centuriin@yandex.ru>
2026-04-04 13:09:04 +0300

    change smoke test

--- Common.Tests/SmokeTests/Game.Tests.cs
+++ Common.Tests/SmokeTests/Game.Tests.cs
@@ -1,8 +1,10 @@
 ﻿using Centuriin.CardGame.Core.Common.Components;
+using Centuriin.CardGame.Core.Common.Components.Zones;
 using Centuriin.CardGame.Core.Common.Entities.Cards;
 using Centuriin.CardGame.Core.Common.Entities.Players;
 using Centuriin.CardGame.Core.Common.Entities.Zones;
 using Centuriin.CardGame.Core.Common.Events;
+using Centuriin.CardGame.Core.Common.Logging;
 using Centuriin.CardGame.Core.Common.Repositories;
 using Centuriin.CardGame.Core.Common.Systems;
 using Centuriin.Centuriin.Core.Common;
@@ -11,9 +13,9 @@ using FluentAssertions;
 
 using Xunit;
 
-namespace Centuriin.CardGame.Core.Common;
+namespace Centuriin.CardGame.Core.Common.SmokeTests;
 
-public sealed class GameSmokeTests
+public sealed class GameTests
 {
     [Fact]
     [Trait("Category", "Smoke")]
@@ -24,13 +26,20 @@ public sealed class GameSmokeTests
         var playerId = new PlayerId(Guid.NewGuid());
 
         var handZone = new Zone(new ZoneId(10));
-        handZone.Add(new OwnerComponent(playerId));
-        handZone.Add(new ZoneRoleComponent(ZoneRole.Hand));
+        handZone.Add(
+            [
+                new OwnerComponent(playerId),
+                new ZoneRoleComponent(ZoneRole.Hand),
+                new HasPrimaryCards(1)
+            ]);
 
         var cardId = new CardId(1);
         var card = new Card(cardId);
-        card.Add(new OwnerComponent(PlayerId.System));
-        card.Add(new ZoneComponent(new ZoneId(0)));
+        card.Add(
+            [
+                new OwnerComponent(PlayerId.System),
+                new ZoneComponent(new ZoneId(0))
+            ]);
 
         var gameState = new GameState(gameId);
         gameState.AddEntity<Player, PlayerId>(new Player(playerId));
@@ -38,27 +47,28 @@ public sealed class GameSmokeTests
         gameState.AddEntity<Zone, ZoneId>(handZone);
         gameState.AddEntity<Card, CardId>(card);
 
-        var movementSystem = new CardMovementSystem();
+        var dealerSystem = new ClassicDealerSystem(DebugLogger.Instance);
+        var movementSystem = new CardMovementSystem(DebugLogger.Instance);
+
         var dispatcher = new EventDispatcher();
         dispatcher.Register<CardDealtEvent>(movementSystem);
+        dispatcher.Register<GameStartedEvent>(dealerSystem);
 
         var eventsRepo = new FakeEventsRepository();
 
         var game = new Game(gameState, eventsRepo, dispatcher);
 
-        var dealer = new ClassicDealerService(game);
-
         // Act
-        await dealer.DealCardsAsync(count: 1, CancellationToken.None);
+        await game.ApplyAsync(new GameStartedEvent(gameId), TestContext.Current.CancellationToken);
 
         // Assert
         var updatedCard = gameState.Get<Card, CardId>(cardId);
         updatedCard.Get<OwnerComponent>().CurrentOwnerId.Should().Be(playerId);
         updatedCard.Get<ZoneComponent>().CurrentZoneId.Should().Be(handZone.Id);
 
-        eventsRepo.Events.Should().HaveCount(2); // CardDealt + GameStarted
-        eventsRepo.Events[0].Should().BeOfType<CardDealtEvent>();
-        eventsRepo.Events[1].Should().BeOfType<GameStartedEvent>();
+        eventsRepo.Events.Should().HaveCount(2); // GameStarted + CardDealt
+        eventsRepo.Events[0].Should().BeOfType<GameStartedEvent>();
+        eventsRepo.Events[1].Should().BeOfType<CardDealtEvent>();
     }
 
     private class FakeEventsRepository : IGameEventsRepository
